f35b464f734aebe062f3f2de09dbb5c714889c89
aminenurgynk <amine.gynk@gmail.com>
2024-11-16 01:55:07 +0300

    router

---
+++ 09-Router/app/src/views/Students.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>Students</h1>
+        <div v-for="student in students" :key="student.id">
+            <router-link :to="{path:`/students/${student.id}`}">{{ student.name }}</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            students: [
+                { id: 1, name: 'Amine', grade: '6' },
+                { id: 2, name: 'fdgdf', grade: '6' },
+                { id: 3, name: 'gfdg', grade: '6' },
+            ]
+        }
+    }
+}
+</script>
+
+<style></style>
