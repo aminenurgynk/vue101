d3b50d9fdaeacdd77e6d39eaff3bda8ffffb8f75
aminenurgynk <amine.gynk@gmail.com>
2024-11-16 22:16:32 +0300

    watch

--- 10-Components/components/src/components/Header.vue
+++ 10-Components/components/src/components/Header.vue
@@ -5,6 +5,10 @@
         <input type="text" v-model="name">
         <input type="text" v-model="surname">
         <p>{{ nameSurname }}</p>
+        <hr>
+        <input type="number" v-model="number1"> <br>
+        <input type="number" v-model="number2"> <br>
+        <input type="number" v-model="total"> <br>
     </div>
 </template>
 
@@ -13,14 +17,29 @@ export default {
     data() {
         return {
             name: '',
-            surname: ''
+            surname: '',
+            number1: 0,
+            number2: 0,
+            total: 0,
         }
     },
     computed: {
         nameSurname() {
             return this.name + ' ' + this.surname
         }
-    }
+    },
+    watch: {
+        number1(newValue, oldValue) {
+            console.log('Numbers', oldValue, newValue);
+            this.total = parseInt(this.number1) + parseInt(this.number2)
+            
+        },
+        number2(newValue, oldValue) {
+            console.log('Numbers', oldValue, newValue);
+            this.total = parseInt(this.number1) + parseInt(this.number2)
+            
+        },
+    },
 }
 </script>
 
