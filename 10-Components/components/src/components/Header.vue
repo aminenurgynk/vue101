52e26a7079577417ccf67c9718354fd46cc41d1c
aminenurgynk <amine.gynk@gmail.com>
2024-11-16 22:04:49 +0300

    computed

--- 10-Components/components/src/components/Header.vue
+++ 10-Components/components/src/components/Header.vue
@@ -1,14 +1,27 @@
 <template>
-  <div>
-    <slot></slot>
-  </div>
+    <div>
+        <slot></slot>
+        <hr>
+        <input type="text" v-model="name">
+        <input type="text" v-model="surname">
+        <p>{{ nameSurname }}</p>
+    </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            name: '',
+            surname: ''
+        }
+    },
+    computed: {
+        nameSurname() {
+            return this.name + ' ' + this.surname
+        }
+    }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
