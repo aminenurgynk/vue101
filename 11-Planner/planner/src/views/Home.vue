55155eaabf6fb762d13330f77dfed07bc0105c97
aminenurgynk <amine.gynk@gmail.com>
2024-11-17 14:36:09 +0300

    data fetch

--- 11-Planner/planner/src/views/Home.vue
+++ 11-Planner/planner/src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <p>{{ project.title }}</p>
+        <p>{{ project.details }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,6 +15,19 @@
 export default {
   name: 'Home',
   components: {
+
+  },
+  data() {
+    return {
+      projects: []
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/projects')
+    .then(res => res.json())
+    .then(data => this.projects = data)
+    .catch(err => console.log(err.message));
+    
   }
 }
 </script>
