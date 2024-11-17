ad374873f8266baadbc86ae5016a76edc9281948
aminenurgynk <amine.gynk@gmail.com>
2024-11-17 15:16:22 +0300

    delete-complete

--- 11-Planner/planner/src/components/SingleProject.vue
+++ 11-Planner/planner/src/components/SingleProject.vue
@@ -1,7 +1,14 @@
 <template>
-  <div class="project">
-    <div @click="showDetails = !showDetails" class="actions">
-        <h3 >{{ project.title }}</h3>
+  <div class="project" :class="{ complete: project.complete }">
+    <div  class="actions">
+        <h3 @click="showDetails = !showDetails" >{{ project.title }}</h3>
+        <div class="icons">
+        <router-link :to="{ name: 'EditProject', params: { id: project.id } }"
+          ><span class="material-icons">edit</span></router-link
+        >
+        <span @click="deleteProject" class="material-icons">delete</span>
+        <span @click="toggleComplete" class="material-icons tick">done</span>
+      </div>
     </div>
     <div v-if="showDetails" class="details">
         <p>{{ project.details }}</p>
@@ -19,7 +26,23 @@ export default {
             showDetails: false,
             uri: "http://localhost:3000/projects/" + this.project.id,
         }
-    }
+    },
+    methods: {
+    deleteProject() {
+      fetch(this.uri, { method: "DELETE" })
+        .then(() => this.$emit("delete", this.project.id))
+        .catch((err) => console.log(err));
+    },
+    toggleComplete() {
+      fetch(this.uri, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ complete: !this.project.complete }),
+      })
+        .then(() => this.$emit("complete", this.project.id))
+        .catch((err) => console.log(err));
+    },
+  },
 }
 </script>
 
